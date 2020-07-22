28ef49d73b506b73e4a86ea2cda7232e5b41ff12
ccalsbeek <caitlin.calsbeek@gmail.com>
2020-07-22 00:52:08 -0700

    added data to table

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(data)
+
+data.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
